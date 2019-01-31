f649e0b66a331439b49dbd88ba1a96dd2972fb1f
Robert Johanson <Robban_9@hotmail.com>
2019-01-31 10:32:17 +0100

    skapade en ny migration

--- Kommunikationsverktyg/Kommunikationsverktyg/Models/DbInitializer/ApplicationDbInitializer.cs
+++ Kommunikationsverktyg/Kommunikationsverktyg/Models/DbInitializer/ApplicationDbInitializer.cs
@@ -7,7 +7,7 @@ using System.Web;
 
 namespace Kommunikationsverktyg.Models.DbInitializer
 {
-    public class ApplicationDbInitializer : System.Data.Entity.CreateDatabaseIfNotExists<ApplicationDbContext>
+    public class ApplicationDbInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<ApplicationDbContext>
     {
         protected override void Seed(ApplicationDbContext db)
         {
