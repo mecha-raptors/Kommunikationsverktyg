07cc27b5ec661513dedc8f1a2209c5b2840a087c
Eric Johansson <johanssoneric94@gmail.com>
2019-02-06 10:45:47 +0100

    migrate

---
+++ Kommunikationsverktyg/Kommunikationsverktyg/Migrations/201902060926404_mig19.cs
@@ -0,0 +1,16 @@
+namespace Kommunikationsverktyg.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig19 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
