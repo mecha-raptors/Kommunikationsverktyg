5b3908133a9103e217fa0305d5fcb6ccac11d997
Anders Husmo <anders.k.husmo@gmail.com>
2019-02-07 13:28:29 +0100

    Updated DateModel

--- Kommunikationsverktyg/Kommunikationsverktyg/Models/DateModel.cs
+++ Kommunikationsverktyg/Kommunikationsverktyg/Models/DateModel.cs
@@ -14,6 +14,7 @@ namespace Kommunikationsverktyg.Models
         public virtual DateTime StartTime { get; set; }
         [Display(Name = "Slutar")]
         public virtual DateTime EndTime { get; set; }
-        public virtual int Votes { get; set; }
+        public virtual int Votes { get => Voters.Count();}
+        public virtual ICollection<ApplicationUser> Voters { get; set; }
     }
 }
