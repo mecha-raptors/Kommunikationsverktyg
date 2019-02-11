8d66f5937c3175ce002e6376707cbbad1d0e8bfb
Samuel lindquist <Samme_li22@hotmail.com>
2019-02-11 15:54:54 +0100

    Followers

---
+++ Kommunikationsverktyg/Kommunikationsverktyg/Models/FollowersModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Kommunikationsverktyg.Models
+{
+    public class FollowersModel
+    {
+        [Key]
+        public virtual int FollowerModelId { get; set; }
+
+        public virtual int CategoryModelId { get; set; }
+        public virtual CategoryModel Category { get; set; }
+
+        public virtual string Id { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
