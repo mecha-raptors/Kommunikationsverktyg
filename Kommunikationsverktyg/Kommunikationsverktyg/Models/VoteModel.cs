f83e5d010c5fa80bde203ebde903e81bc97c8d83
Anders Husmo <anders.k.husmo@gmail.com>
2019-02-13 09:51:12 +0100

    VoteModel

---
+++ Kommunikationsverktyg/Kommunikationsverktyg/Models/VoteModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kommunikationsverktyg.Models
+{
+    public class VoteModel
+    {
+        public int Id { get; set; }
+        public bool VoteFor { get; set; }
+        public ApplicationUser Voter { get; set; }
+        public DateModel Date { get; set; }
+    }
+}
